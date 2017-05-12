73be6943d7983f247ebd5bd9d83777c4cb4e9448
team108 <hytzf8888@gmail.com>
2017-05-12 21:27:37 +0800

    增加php版本判断。

--- face.cc
+++ face.cc
@@ -175,7 +175,11 @@ PHP_FUNCTION(confirm_face_compiled)
 	}
 
 	len = spprintf(&strg, 0, "Congratulations! You have successfully modified ext/%.78s/config.m4. Module %.78s is now compiled into PHP.", "face", arg);
+	#if PHP_MAJOR_VERSION < 7
 	RETURN_STRINGL(strg, len, 0);
+	#else
+	RETURN_STRINGL(strg, len);
+	#endif
 }
 /* }}} */
 /* The previous line is meant for vim and emacs, so it can correctly fold and 
